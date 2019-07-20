5e987e0065a502893b1137c62651c90f8fdfc280
George Gach <georgeggach@gmail.com>
2019-07-20 12:24:59 -0400

    added gh-pages.sh

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+directory=_gh-pages
+branch=gh-pages
+build_command() {
+  cp -r flowiz/gui/web _gh-pages
+}
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf $directory
+
+echo -e "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
+
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Deploy updates" &&
+  git push origin $branch
+
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove $directory
